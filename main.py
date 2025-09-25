9866c301ea03c4cebbabd9c9896f80a0f079d111
AlCatSplat <83360294+AlCatSplat@users.noreply.github.com>
2025-09-25 03:04:25 -0700

    Added a simple addition command.

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-# This example requires the 'message_content' intent.
-
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command(help="Calculates the sum of two numbers.")
+async def calc(ctx, arg: int = commands.parameter(description="The first number to be added"), arg2: int = commands.parameter(description="The second number to be added")):
+    await ctx.send('The result is ' + str((arg + arg2)))
 
-load_dotenv() # load .env for token to be used below.
-client.run(os.getenv('DISCORD_TOKEN'))
+
+load_dotenv()  # load .env for token to be used below.
+bot.run(os.getenv('DISCORD_TOKEN'))
